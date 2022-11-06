975da5e840b6a6ba965c784ff5a113fff12313a3
Uk <qmdlwd2@gmail.com>
2022-11-06 16:22:27 +0900

    로그 찍어보기

--- app/src/main/java/com/thirdparty/kotlinbasic_uk/MainActivity.kt
+++ app/src/main/java/com/thirdparty/kotlinbasic_uk/MainActivity.kt
@@ -2,12 +2,21 @@ package com.thirdparty.kotlinbasic_uk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        clickBtn.setOnClickListener {
+
+//         이 { } 안 에 있는 코드만, 버튼이 눌렀을때 실행됨.
+
+            Log.d("메인화면로그", "클릭용 버튼 눌림")
+
+        }
 
 
     }
