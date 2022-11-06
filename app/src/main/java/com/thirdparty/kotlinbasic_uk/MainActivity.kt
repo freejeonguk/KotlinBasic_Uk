f95413ff16cb935225a3f291a77bd98cee1206b7
Uk <qmdlwd2@gmail.com>
2022-11-06 16:35:07 +0900

    토스트 띄워보기

--- app/src/main/java/com/thirdparty/kotlinbasic_uk/MainActivity.kt
+++ app/src/main/java/com/thirdparty/kotlinbasic_uk/MainActivity.kt
@@ -3,6 +3,7 @@ package com.thirdparty.kotlinbasic_uk
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,9 +16,12 @@ class MainActivity : AppCompatActivity() {
 //         이 { } 안 에 있는 코드만, 버튼이 눌렀을때 실행됨.
 
             Log.d("메인화면로그", "클릭용 버튼 눌림")
-            Log.e("메인화면로그","에러 관련 로그")
+            Log.e("메인화면로그", "에러 관련 로그")
 
+        }
 
+        smallBtn.setOnClickListener {
+            Toast.makeText(this, "작은 버튼 눌림", Toast.LENGTH_SHORT).show()
         }
 
 
